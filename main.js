13400cb9eecfead7506b620094de57b295422465
bSchutters <bryan021@hotmail.be>
2023-10-12 11:58:32 +0200

    fixes +links

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 import "/css/style.css";
 import "/css/reset.css";
 import "/css/responsive.css";
+
+const iconBurger = document.getElementById("btn-menu-burger");
+const navBurger = document.getElementById("burger-menu");
+const body = document.getElementsByTagName("body")[0];
+
+if (iconBurger) {
+  iconBurger.onclick = function () {
+    navBurger.classList.toggle("test");
+    iconBurger.classList.toggle("bx-menu");
+    iconBurger.classList.toggle("bx-x");
+  };
+}
